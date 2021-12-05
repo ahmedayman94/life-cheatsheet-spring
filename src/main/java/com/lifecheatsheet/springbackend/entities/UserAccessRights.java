84ecf88c1267ebd22600a58503a9c2c94a7fb1b9
Ahmed Moustafa <ahmedmou@kth.se>
2021-12-05 14:01:28 +0100

    user access rights

---
+++ src/main/java/com/lifecheatsheet/springbackend/entities/UserAccessRights.java
@@ -0,0 +1,7 @@
+package com.lifecheatsheet.springbackend.entities;
+
+public class UserAccessRights {
+    public static String readonly ="readonly";
+    public static String publisher ="publisher";
+    public static String admin ="admin";
+}
