73eb3c79c8ac1d3230cdb58a26d6857602863bd3
Ahmed Moustafa <ahmedmou@kth.se>
2021-12-05 14:01:16 +0100

    token Service to take care of decoding/verifying jwt

---
+++ src/main/java/com/lifecheatsheet/springbackend/services/TokenService.java
@@ -0,0 +1,55 @@
+package com.lifecheatsheet.springbackend.services;
+
+
+import com.auth0.jwk.Jwk;
+import com.auth0.jwk.JwkException;
+import com.auth0.jwk.JwkProvider;
+import com.auth0.jwk.JwkProviderBuilder;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.lifecheatsheet.springbackend.entities.User;
+import org.springframework.stereotype.Service;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.security.interfaces.RSAPublicKey;
+
+@Service
+public class TokenService {
+    static final String GOOGLE_JWKS_URI = "https://www.googleapis.com/oauth2/v3/certs";
+    private final JwkProvider provider;
+
+    public TokenService() throws MalformedURLException {
+        provider = new JwkProviderBuilder(new URL(GOOGLE_JWKS_URI))
+                .build();
+    }
+
+    public void verifyTokenByGoogle(String token) throws JwkException {
+        try {
+            DecodedJWT jwt = JWT.decode(token);
+            Jwk jwk = provider.get(jwt.getKeyId());
+            Algorithm algorithm = Algorithm.RSA256((RSAPublicKey) jwk.getPublicKey(), null);
+            JWTVerifier verifier = JWT.require(algorithm)
+                    .withIssuer("accounts.google.com")
+                    .build();
+            verifier.verify(token);
+        } catch (JWTVerificationException exception) {
+            // @TODO: Log the error
+
+            throw exception;
+        }
+    }
+
+    public User extractUserFromToken(final String token) {
+        DecodedJWT jwt = JWT.decode(token);
+
+        String email = jwt.getClaim("email").asString();
+        String firstName = jwt.getClaim("given_name").asString();
+        String lastName = jwt.getClaim("family_name").asString();
+
+        return new User(firstName, lastName, email);
+    }
+}
