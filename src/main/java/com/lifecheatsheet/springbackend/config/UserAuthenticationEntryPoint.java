18718aa61c6f692ffffb18af7a5b9bb4fec69c88
Ahmed Moustafa <ahmedmou@kth.se>
2021-12-12 22:50:44 +0100

    send 401 without body if authentication fails

--- src/main/java/com/lifecheatsheet/springbackend/config/UserAuthenticationEntryPoint.java
+++ src/main/java/com/lifecheatsheet/springbackend/config/UserAuthenticationEntryPoint.java
@@ -6,20 +6,13 @@ import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Component
 public class UserAuthenticationEntryPoint implements AuthenticationEntryPoint {
-    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
-
     @Override
     public void commence(HttpServletRequest request,
                          HttpServletResponse response,
-                         AuthenticationException authException)
-            throws IOException {
+                         AuthenticationException authException) {
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-
-        OBJECT_MAPPER.writeValue(response.getOutputStream(), null);
     }
 }
