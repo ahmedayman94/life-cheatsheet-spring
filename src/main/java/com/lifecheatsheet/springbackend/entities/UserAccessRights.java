085922d0cbef47e4e25766b3d4b5a734c5ee9a4f
Ahmed Moustafa <ahmedmou@kth.se>
2021-12-17 21:46:56 +0100

    change constant to all caps

--- src/main/java/com/lifecheatsheet/springbackend/entities/UserAccessRights.java
+++ src/main/java/com/lifecheatsheet/springbackend/entities/UserAccessRights.java
@@ -1,7 +1,7 @@
 package com.lifecheatsheet.springbackend.entities;
 
 public class UserAccessRights {
-    public static String readonly ="readonly";
-    public static String publisher ="publisher";
-    public static String admin ="admin";
+    public static String READ_ONLY ="readonly";
+    public static String PUBLISHER ="publisher";
+    public static String ADMIN ="admin";
 }
