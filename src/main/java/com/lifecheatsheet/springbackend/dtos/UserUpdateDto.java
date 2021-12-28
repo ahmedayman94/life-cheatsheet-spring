d07bcc5368358b89931450a617c50e85a08f1dd8
ahmedayman94 <ahmedmou@kth.se>
2021-12-28 15:11:13 +0200

    restriction on name length

--- src/main/java/com/lifecheatsheet/springbackend/dtos/UserUpdateDto.java
+++ src/main/java/com/lifecheatsheet/springbackend/dtos/UserUpdateDto.java
@@ -1,7 +1,12 @@
 package com.lifecheatsheet.springbackend.dtos;
 
+import org.hibernate.validator.constraints.Length;
+
 public class UserUpdateDto {
+    @Length(min = 0, max = 30)
     private String firstName;
+
+    @Length(min = 0, max = 30)
     private String lastName;
 
     public String getFirstName() {
