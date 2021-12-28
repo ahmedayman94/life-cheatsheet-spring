5b9a06ff509f65f26f317d40498bd4546afd2537
ahmedayman94 <ahmedmou@kth.se>
2021-12-28 15:11:50 +0200

    make get methods public for /categories

--- src/main/java/com/lifecheatsheet/springbackend/config/SecurityConfig.java
+++ src/main/java/com/lifecheatsheet/springbackend/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.lifecheatsheet.springbackend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -25,6 +26,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
                 .authorizeRequests()
                 .antMatchers(LOGIN_URL).permitAll()
+                .antMatchers(HttpMethod.GET, "/api/v1/categories/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
 
